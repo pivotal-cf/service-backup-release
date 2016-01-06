cef2668bc80e647b8318ec12039f076c181c967a
Craig Furman <cfurman@pivotal.io>
2016-01-06 10:35:18 +0000

    create final release concourse job
    * triggered by S3 file containing SHA
    * uploads tarball to S3
    
    Signed-off-by: Houssem El Fekih <hfekih@pivotal.io>

---
+++ ci/scripts/create_final_release.sh
@@ -0,0 +1,47 @@
+#!/bin/bash -e
+
+mustHave() {
+  var=$1
+  shift
+
+  if [ -z "${!var}" ]
+  then
+    echo "must set $var" >&2
+    exit 1
+  fi
+}
+
+for v in AWS_ACCESS_KEY_ID AWS_SECRET_ACCESS_KEY GIT_SSH_KEY
+do
+  mustHave $v
+done
+
+pushd $(dirname $0)/../..
+
+sha_to_release=$(cat ../sha-to-release/service-backup-release.sha)
+echo "releasing $sha_to_release"
+git checkout $sha_to_release
+
+release_version=$(( $(ls releases/service-backup | grep service-backup | wc -l) + 1 ))
+git tag "v${release_version}"
+
+printf "\-\-\-\nblobstore:\n\ts3:\n\t\taccess_key_id: ${AWS_ACCESS_KEY_ID}\n\t\tsecret_access_key: ${AWS_SECRET_ACCESS_KEY}" > config/private.yml
+bosh -n create release --name service-backup --final --with-tarball
+
+key=/tmp/key
+echo "$GIT_SSH_KEY" > $key
+eval "$(ssh-agent)"
+chmod 400 $key
+ssh-add $key
+
+git config --global user.email "cf-london-eng+enablement@pivotal.io"
+git config --global user.name "CF Services Enablement CI server"
+git add releases
+git add .final_builds
+git commit -m "Final release v${release_version} metadata"
+
+git fetch
+git rebase origin/develop
+git push origin HEAD:develop
+
+git push origin --tags
