3acdcd71f6afbc177f5a27dfec9bd05248235dd6
Jatin Naik <jnaik@pivotal.io>
2016-02-17 16:02:26 +0000

    append timestamp to ci releases
    Signed-off-by: Tom Anderson <toanderson@pivotal.io>

--- ci/scripts/create_dev_release.sh
+++ ci/scripts/create_dev_release.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -e
 
 cd $(dirname $0)/../..
-bosh -n create release --name service-backup --with-tarball
+bosh -n create release --name service-backup --with-tarball --timestamp-version
