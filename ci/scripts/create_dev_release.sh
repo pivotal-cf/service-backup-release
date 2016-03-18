f64e394d4de803107adb85067fd9071d8ca1c7b7
Jatin Naik <jnaik@pivotal.io>
2016-03-18 14:39:56 +0000

    Fix the upload of BOSH releases in CI
    Signed-off-by: Tom Anderson <toanderson@pivotal.io>

--- ci/scripts/create_dev_release.sh
+++ ci/scripts/create_dev_release.sh
@@ -1,6 +1,8 @@
 #!/bin/bash -eu
 
+export BOSH_CA_CERT_PATH=$(readlink -f $BOSH_CA_CERT_PATH)
+
 cd $(dirname $0)/../..
-bosh --non-interactive create release --name service-backup --with-tarball --timestamp-version
+bosh --non-interactive create release --name service-backup --timestamp-version
 bosh --non-interactive --target $BOSH_URL --ca-cert $BOSH_CA_CERT_PATH cleanup
 bosh --non-interactive --target $BOSH_URL --ca-cert $BOSH_CA_CERT_PATH upload release
