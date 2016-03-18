a83a768041fb1b87241431b29be15d29b778d948
Tom Anderson <toanderson@pivotal.io>
2016-03-18 15:08:38 +0000

    Fix invocation of bosh deploy
    Signed-off-by: Jatin Naik <jnaik@pivotal.io>

--- ci/scripts/deploy_release.sh
+++ ci/scripts/deploy_release.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -eu
 
 cd $(dirname $0)/../..
-bosh --non-interactive --target $BOSH_URL --ca-cert $BOSH_CA_CERT_PATH deploy --deployment $MANIFEST
+bosh --non-interactive --target $BOSH_URL --ca-cert $BOSH_CA_CERT_PATH --deployment $MANIFEST deploy
