146d7d57c24b6b39561089124a39d6bc790ec5c1
Jatin Naik <jnaik@pivotal.io>
2016-03-18 15:14:21 +0000

    Fixed cert path for bosh deploy
    Signed-off-by: Tom Anderson <toanderson@pivotal.io>

--- ci/scripts/deploy_release.sh
+++ ci/scripts/deploy_release.sh
@@ -1,4 +1,6 @@
 #!/bin/bash -eu
 
+export BOSH_CA_CERT_PATH=$(readlink -f $BOSH_CA_CERT_PATH)
+
 cd $(dirname $0)/../..
 bosh --non-interactive --target $BOSH_URL --ca-cert $BOSH_CA_CERT_PATH --deployment $MANIFEST deploy
