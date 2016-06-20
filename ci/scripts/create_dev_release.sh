e69c360900a4b7ea63e398c0620f36489f06f7b8
Craig Furman <cfurman@pivotal.io>
2016-06-20 11:28:07 +0100

    move system tests to bosh lite
    also use standard script to upload releases, and bosh deployment resource to deploy

--- ci/scripts/create_dev_release.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash -eu
-
-export BOSH_CA_CERT_PATH=$(readlink -f $BOSH_CA_CERT_PATH)
-
-cd $(dirname $0)/../..
-bosh --non-interactive create release --name service-backup --timestamp-version
-bosh --non-interactive --target $BOSH_URL --ca-cert $BOSH_CA_CERT_PATH cleanup
-bosh --non-interactive --target $BOSH_URL --ca-cert $BOSH_CA_CERT_PATH upload release
--- ci/scripts/deploy_release.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash -eu
-
-bosh --non-interactive --target $BOSH_URL --ca-cert $BOSH_CA_CERT_PATH --deployment $MANIFEST deploy
